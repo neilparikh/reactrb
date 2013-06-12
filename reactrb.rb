20345305826765ec54fd96bfa4c8c4d161847c94
Neil Parikh <parikh.neil@me.com>
2013-06-11 19:13:19 -0600

    Get rid of example from implementation file

--- reactrb.rb
+++ reactrb.rb
@@ -45,15 +45,3 @@ class ReactRB
     return self.value.to_s
   end
 end
-
-# Quick Example
-a = ReactRB.new(1)
-b = ReactRB.new(2)
-c = ReactRB.new.depends_on(a, b) do |var1, var2|
-  var1 + var2**2
-end
-puts c #=> 5
-
-a.value = 10
-b.value = 5
-puts c #=> 35
